a4975883de797f81d6da692407406d6e379f9e73
dhichiafis <91750915+dhichiafis@users.noreply.github.com>
2024-08-18 13:23:26 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {RouterLink,RouterView} from 'vue-router'
+import axios from 'axios'
+import {useStore} from 'vuex'
+import { onBeforeMount } from 'vue'
+const store=useStore()
+onBeforeMount(()=>{
+  const token=store.state.token 
+  if(token){
+    axios.defaults.headers.common['Authorization']='token'+token 
+  }else{
+    axios.defaults.headers.common['Authorization']=''
+  }
+})
+</script>
+<template>
+  <div>
+    <RouterView></RouterView>
+  </div>
+</template>
